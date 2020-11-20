bb878d843ed2bb50576b9809a2b92fd7e9abb627
Ian <ianjduran@gmail.com>
2020-11-19 21:23:34 -0400

    Documenté el codigo y reemplacé secciones de código por funciones

--- avanceZonasDerrape.m
+++ avanceZonasDerrape.m
@@ -53,14 +53,14 @@ while(ecuationFound==false)
         
     % Se crea una funcion simbólica de la curva
     syms x
-    eqn = coef(1)*x^3 + coef(2)*x^2 + coef(3)*x + coef(4);
+    ecuacion_modelo = coef(1)*x^3 + coef(2)*x^2 + coef(3)*x + coef(4);
     
     % Se determina los puntos críticos de la función
-    puntosCriticos = double(solve(diff(eqn,x)));
+    [punto_min_max_1, punto_min_max_2] = find_min_max_points(ecuacion_modelo);
     
     % Se calcula la longitud de arco y el radio de curvatura de la función
-    arco = calc_arc(eqn, X0, XF);
-    radio = calc_radius(eqn,puntosCriticos(1));
+    arco = calc_arc(ecuacion_modelo, X0, XF);
+    radio = calc_radius(ecuacion_modelo,punto_min_max_1);
     
     % Se verifica si la ecuacion cumple con las condiciones necesarias
          % Curvatura de radio < 50
@@ -77,7 +77,7 @@ end
 %% IMPRESIÓN DE DATOS IMPORTANTES
 
 disp("Ecuacion:");
-disp(eqn);
+disp(ecuacion_modelo);
 disp("Lista de coeficientes:");
 fprintf("a3: %.10f\n", coef(1));
 fprintf("a2: %.7f\n", coef(2));
@@ -86,7 +86,9 @@ fprintf("a0: %f\n\n", coef(4));
 disp("Longitud:");
 disp(arco);
 disp("Puntos Críticos:");
-disp(double(puntosCriticos));
+disp("Minimo Funcion: " + num2str(punto_min_max_1) )
+disp("Maximo Funcion: " + num2str(punto_min_max_2) )
+
 disp("Radio de Curvatura:");
 disp(radio);
 disp("Coordenadas: ")
@@ -96,7 +98,7 @@ disp("(" + num2str(X2) + "," + num2str(Y2)+ ")");
 %% GRAFICACION DE LA CURVA
 
 % Se grafica la curva, con limites del punto inicial y final
-fplot(eqn, [X0 XF], 'lineWidth', 1)
+fplot(ecuacion_modelo, [X0 XF], 'lineWidth', 1)
 
 % Se establecen los límites de la figura
 %axis([(X0-50) (XF+50) 0 400])
@@ -107,23 +109,23 @@ xlim([-40,400])
 hold on
 
 % Se dibujan los puntos de las coordenadas 
-plot(X0,Y0, 'r.', 'lineWidth', 5);
-plot(XF,YF, 'k.', 'lineWidth', 5);
-plot(X1,Y1, 'b.', 'lineWidth', 5);
-plot(X2,Y2, 'b.', 'lineWidth', 5);
+plot(X0,Y0, 'ro');
+plot(XF,YF, 'bo');
+plot(X1,Y1, 'ko');
+plot(X2,Y2, 'ko');
 
 
 
 %% DIBUJO DE CIRCULOS SOBREPUESTOS EN LA GRÁFICA
 
 % Convierte la función simbólica en una función anónima
-ecuacionCurva = matlabFunction(eqn);
+ecuacionCurva = matlabFunction(ecuacion_modelo);
 
 % Se evalua la ecuación en los puntos críticos para obtener la coordenada
 % de los centros de los círculos (Se obtiene el centro en el punto crítico,
 % por lo que es necesario recorrer el centro)
-xCentro1 = puntosCriticos(1);
-xCentro2 = puntosCriticos(2);
+xCentro1 = punto_min_max_1;
+xCentro2 = punto_min_max_2;
 yCentro1 = ecuacionCurva(xCentro1);
 yCentro2 = ecuacionCurva(xCentro2);
 
@@ -152,40 +154,47 @@ if(xCentro2>X0 && xCentro2 < XF)
     plot(xunit+xCentro2, yunit+yCentro2,'g');
 end
 
-funcionCurva = matlabFunction(eqn);
+% Se convierte la ecuacion en una función anónima para evaluar la función
+% en distintos puntos
+funcionCurva = matlabFunction(ecuacion_modelo);
 
+%% BUSQUEDA Y GRAFICACIÓN DE ZONAS DE DERRAPE
 
+% PRIMERA ZONA DE DERRAPE
+
+% Se inicializa la lista de coordenadas que tiene la primera zona de 
+% derrape
 zonaDerrapeX1 = [];
-
-if(puntosCriticos(1)>X0)
-    for punto = puntosCriticos(1):-1:X0
-        zonaDerrapeX1 = [punto zonaDerrapeX1];
-        if(calc_radius(eqn, punto)>=50)
-            break;
-        end
-    end
-    zonaDerrapeY1 = funcionCurva(zonaDerrapeX1);
+zonaDerrapeY1 = [];
+% Se ejecuta si es que el punto mínimo local se encuentra dentro de la
+% curva
+if(punto_min_max_1>X0)
+    % Se considera hallar desde el primer punto crítico, al principio de la
+    % curva, ya que el carro viene desde la izquierda.
+    [zonaDerrapeX1, zonaDerrapeY1]= hallar_zona_derrape(ecuacion_modelo, ...
+    punto_min_max_1, X0);
     plot(zonaDerrapeX1,zonaDerrapeY1, 'r','LineWidth',2);
 end
 
+% SEGUDNA ZONA DE DERRAPE
 
+% Inicialización de la lista de coordenadas
 zonaDerrapeX2 = [];
-if(puntosCriticos(2)<XF)
-    for punto = puntosCriticos(2):-1:puntosCriticos(1)
-        zonaDerrapeX2 = [punto zonaDerrapeX2];
-        if(calc_radius(eqn,punto)>=50)
-            break;
-        end
-    end
-   
-    zonaDerrapeY2 = funcionCurva(zonaDerrapeX2); 
+zonaDerrapeY2 = [];
+
+if(punto_min_max_2<XF)
+     [zonaDerrapeX2, zonaDerrapeY2] = hallar_zona_derrape(ecuacion_modelo, ...
+     punto_min_max_2, X0);
     plot(zonaDerrapeX2,zonaDerrapeY2, 'r','LineWidth',2);
 end
 
+% Se despliega información sobre la cantidad de coordenadas que existe en
+% las zonas de derrape
 disp("Zona Derrape 1:")
 disp(length(zonaDerrapeX1));
 disp("Zona Derrape2: ")
 disp(length(zonaDerrapeX2));
+
 
 
 hold off
@@ -195,14 +204,50 @@ legend('Curva','Punto inicial','Punto final','Punto intermedio', ...
        'Punto intermedio','Circulos que definen la curvatura')
 
 
-% Puede escribir los resultados en un archivo
+% En caso que se requiera, se pueden almacenar los resultados en un archivo
 %{
-fileID = fopen('coefs.txt','w');
+fileID = fopen('informacionRelevante.txt','w');
 fprintf(fileID,'%s\n',eqn);
 fclose(fileID);
 %}
 
 %% DEFINICION DE FUNCIONES
+
+% Función que halla las listas de la coordenadas de la zona de derrape de
+% la función
+
+function [lista_coordenadas_x, lista_coordenadas_y] = ...
+                               hallar_zona_derrape(eqn, ...
+                               punto_final, punto_inicial)
+    % Convierte la ecuacion para evaluarla en distintos puntos
+    funcion_curva = matlabFunction(eqn);
+    % Se inicializa la lista;
+    lista_x = [];
+    % Se recorre un ciclo que recorre de manera inversa los puntos, para
+    % hallar desde el lado izquierdo el area de curvatura.
+    for punto = punto_final:-1:punto_inicial
+        % Se añade un elemento al principio de la lista
+        lista_x = [punto lista_x];
+        % Se deja de registrar los elementos que tengan una curvatura
+        % mayor a 50
+        if(calc_radius(eqn,punto)>50)
+            lista_coordenadas_x = lista_x;
+            break;
+        end
+    end
+    % Se obtiene la lista de coordenadas en Y al evaluar la lista de
+    % coordenadas en X
+    lista_coordenadas_y = funcion_curva(lista_coordenadas_x);
+end
+
+%Funcion que halla y devuelve los puntos maximos y minimos locales de la
+% ecuación
+
+function [punto_1, punto_2] = find_min_max_points(eqn)
+    criticPoints = double(solve(diff(eqn)));
+    punto_1 = criticPoints(1);
+    punto_2 = criticPoints(2);
+end
 
 % Funcion que calcula la longitud de una ecuacion dados sus limites 
 function arc = calc_arc(func,a,b)
